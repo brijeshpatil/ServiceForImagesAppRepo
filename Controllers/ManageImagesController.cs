280b72927270b3c300f68dac60b391d7a89edf56
brijeshpatil <brijesh.patil@tops-int.com>
2017-01-17 13:10:06 +0530

    Uploaded Project Files

---
+++ Controllers/ManageImagesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServiceForImages.MyDB;
+
+namespace ServiceForImages.Controllers
+{
+    public class ManageImagesController : ApiController
+    {
+
+        ServiceForImagesDBEntities db = new ServiceForImagesDBEntities();
+
+        [AcceptVerbs("GET","POST")]
+        public string ImageInString(string ImageData)
+        {
+            StoreImageInString st = new StoreImageInString();
+            st.ImgData = ImageData;
+            db.StoreImageInStrings.Add(st);
+            db.SaveChanges();
+            return "status={Record Saved}";
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public string ImageInBinary(byte[] ImageData)
+        {
+            StoreImageInBinary st = new StoreImageInBinary();
+            st.ImgData = ImageData;
+            db.StoreImageInBinaries.Add(st);
+            db.SaveChanges();
+            return "status={Record Saved}";
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public List<StoreImageInString> AllImagesInStringFormat()
+        {
+            return db.StoreImageInStrings.ToList();
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public List<StoreImageInBinary> AllImagesInBinaryFormat()
+        {
+            return db.StoreImageInBinaries.ToList();
+        }
+
+        //[HttpPost]
+        //public string PostNewImageWIthString(string ImageData)
+        //{
+        //    StoreImageInString st = new StoreImageInString();
+        //    st.ImgData = ImageData;
+        //    db.StoreImageInStrings.Add(st);
+        //    db.SaveChanges();
+        //    return "status={Record Saved}";
+        //}
+    }
+}
